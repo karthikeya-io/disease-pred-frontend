a1ba200ac5cfa9c0c820af47b09b4d6a8b72e8ba
karthikeya-io <karthikeyabethu01@gmail.com>
2022-12-03 16:06:11 +0530

    connected to backend

--- src/components/MySearchComponent.jsx
+++ src/components/MySearchComponent.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
 import '../css/Searchbar.css';
+import { useState } from 'react'
 
 const MySearchComponent = () => {
+
+    const [formDetails, setFormDetails] = useState({ "search": "" })
+    const changeHandler = (event) => {
+        let name = event.target.name;
+        console.log(event.target.value);
+        setFormDetails((prev) => {
+            return (
+                {
+                    ...prev,
+                    [name]: event.target.value
+                }
+            )
+        })
+    }
+
+
+    async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        alert(JSON.stringify(formDetails))
+        console.log(formDetails);
+        postData('http://localhost:4000/api/search', formDetails)
+            .then((data) => {
+                console.log(data); // JSON data parsed by `data.json()` call
+                setFormDetails({ "search": "" });
+            });
+    }
 
     return (
         <div>
             <h2>Search for Diseases based on symptoms</h2>
-            <form className="example" action="/action_page.php">
-                <input type="text" placeholder="Search.." name="search"/>
-                    <button type="submit"><i className="fa fa-search"></i></button>
+            <form className="example" onSubmit={submitHandler}>
+                <input onChange={changeHandler} type="text" placeholder="Search.." name="search" />
+                <button type="submit"><i className="fa fa-search"></i></button>
             </form>
         </div>
     )
