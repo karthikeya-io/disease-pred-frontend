50a45bcadcc1b929225d3b25e5c03661d6c89ed7
karthikeya-io <karthikeyabethu01@gmail.com>
2022-12-06 19:43:33 +0530

    feedback capture and post to sevice

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,7 +2,57 @@ import React from 'react'
 import "../css/Result.css"
 
 export default function Result(props) {
-    const array = []
+    let array = []
+
+    const feedbackHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        const found = array.findIndex(element => element.name === name )
+        const feedbackDisease = {
+            name: name,
+            value: value
+        }
+        if (found == -1) {
+            array.push(feedbackDisease)
+        }
+        else {
+            array[found].value = value
+        }
+        
+        
+    }
+
+    async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const feedback = {
+            arr: array
+        }
+        alert(JSON.stringify(feedback))
+        console.log(feedback);
+        postData('http://localhost:4000/api/feedback', feedback)
+            .then((data) => {
+                console.log(data); // JSON data parsed by `data.json()` call
+                array = []
+            });
+    }
     return (
         <div style={{ margin: " 20px" }}>
             <h3>Search Results</h3>
@@ -16,10 +66,10 @@ export default function Result(props) {
                                         <div className='card-text'>
                                             <span className='resultName'>{res._source.disease}</span>
                                             <span className='options'>
-                                                <input type="radio" name={res._source.disease} id="yes" />
+                                                <input onChange={feedbackHandler} className='radio' type="radio" value='yes' name={res._source.disease}  />
                                                 <label htmlFor="yes">Yes</label>
 
-                                                <input type="radio" name={res._source.disease} id="No" />
+                                                <input onChange={feedbackHandler} className='radio'  type="radio" value='no' name={res._source.disease}  />
                                                 <label htmlFor="No">No</label>
                                             </span>
                                         </div>
@@ -31,7 +81,7 @@ export default function Result(props) {
                     })
                 }
             </div>
-
+            <button style={{ margin: " 20px" }} onClick={submitHandler} className='btn btn-primary'>submit</button>
         </div>
     )
 }
